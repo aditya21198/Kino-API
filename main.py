a833769b35295e94fab58931027fc7f6957fe115
aditya21198 <aditya.perdana@samb.co.id>
2026-02-03 10:58:38 +0700

    fix : kino send bulk monthly

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from fastapi import FastAPI, BackgroundTasks
-from models import ManualPostInvoiceKino,KinoPostStock
+from models import ManualPostInvoiceKino,KinoPostStock,ManualSendData
 from services import end_of_month_job,end_of_the_day_invoice_job
-from kino.kino_api import ids_post_invoice,post_stock
+from kino.kino_api import ids_post_invoice,post_stock,manual_send_data
 
 app = FastAPI()
 
@@ -28,3 +28,13 @@ async def ids_post_stock(payload: KinoPostStock,background_tasks:BackgroundTasks
         "status": "processing",
         "message": "Request accepted and is being processed in background."
     }
+
+@app.post("/manual_send_data")
+async def mannual_send_data(payload:ManualSendData,background_task:BackgroundTasks):
+    data_dict = payload.dict()
+    background_task.add_task(manual_send_data,data_dict)
+    return{
+        "status": "processing",
+        "message": "Request accepted and is being processed in background."
+    }
+
