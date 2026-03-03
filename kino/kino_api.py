e19e83b5169b6b7cce2bd236c82faf68b566e5d1
aditya21198 <aditya.perdana@samb.co.id>
2026-03-04 01:00:19 +0700

    fix : send kino

--- kino/kino_api.py
+++ kino/kino_api.py
@@ -949,12 +949,15 @@ def worker_send_invoice(raw_payloads, is_cancel=False, stock_max_life=None, stoc
                     "QTY": stock_item["QTY"] + qty
                 })
             
-            if (new_payload_maxlife["DATA"][0]["DETAIL"] or
-                new_payload_non_maxlife["DATA"][0]["DETAIL"]):
+            if new_payload_maxlife["DATA"][0]["DETAIL"]:
                 
                 post_stock_payload(
                     header_maxlife=new_payload_maxlife,
-                    header_without_maxlife=new_payload_non_maxlife
+                )
+                
+            if new_payload_non_maxlife["DATA"][0]["DETAIL"]:
+                post_stock_payload(
+                    header_without_maxlife==new_payload_non_maxlife,
                 )
 
             if is_cancel:
