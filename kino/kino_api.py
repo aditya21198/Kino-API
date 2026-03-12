72b97c3374616641e6265469ef9fa7bf5eab06d4
aditya21198 <aditya.perdana@samb.co.id>
2026-03-12 14:04:57 +0700

    fix : kino iso format date

--- kino/kino_api.py
+++ kino/kino_api.py
@@ -280,7 +280,8 @@ def check_trasaction_date_over_closing_date_kino(transaction_date, dn_posting_da
             new_transaction_date = transaction_date
         elif dn_posting_date > today_month_day2:
             new_transaction_date = date(today.year,today.month,1)
-    return new_transaction_date
+            new_transaction_date = new_transaction_date
+    return new_transaction_date.isoformat()
 
     
 
