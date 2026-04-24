3a08e9729425bb39633faf912af13392539588fd
aditya21198 <aditya.perdana@samb.co.id>
2026-04-24 10:13:51 +0700

    fix : kino api function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from fastapi import FastAPI, BackgroundTasks
 from models import ManualPostInvoiceKino,KinoPostStock,ManualSendData,LoadDBPCache
 from services import end_of_month_job,end_of_the_day_invoice_job,post_load_dbp_asi
-from kino.kino_api import ids_post_invoice,post_stock,manual_send_data
+from kino.new_kino_api import ids_post_invoice,post_stock
 
 app = FastAPI()
 
@@ -25,15 +25,6 @@ async def ids_post_stock(payload: KinoPostStock,background_tasks:BackgroundTasks
     data_dict = payload.dict()
     background_tasks.add_task(post_stock, data_dict)
     return {
-        "status": "processing",
-        "message": "Request accepted and is being processed in background."
-    }
-
-@app.post("/manual_send_data")
-async def mannual_send_data(payload:ManualSendData,background_task:BackgroundTasks):
-    data_dict = payload.dict()
-    background_task.add_task(manual_send_data,data_dict)
-    return{
         "status": "processing",
         "message": "Request accepted and is being processed in background."
     }
