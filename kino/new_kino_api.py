b60bcb023d6b270440f764655a8a2081abb0834c
aditya21198 <aditya.perdana@samb.co.id>
2026-04-30 10:51:53 +0700

    fix : kino replace dummy items

--- kino/new_kino_api.py
+++ kino/new_kino_api.py
@@ -481,6 +481,17 @@ def get_sub_brand_mapping():
 
     return data
 
+def append_or_sum_detail(detail_list, stock_detail):
+    for d in detail_list:
+        if (
+            d["PRDCODE"] == stock_detail["PRDCODE"]
+            and d["WHLOC1"] == stock_detail["WHLOC1"]
+            and d["WHLOC2"] == stock_detail["WHLOC2"]
+        ):
+            d["QTY"] += stock_detail["QTY"]
+            return
+    detail_list.append(stock_detail)
+
 def create_stock_payload(item_code: str = None, warehouse: str = None,date:str=None):
     try:
         from services import kino_get_replacement_item
@@ -533,9 +544,11 @@ def create_stock_payload(item_code: str = None, warehouse: str = None,date:str=N
                                         detail = header_template[header]["DATA"][0]["DETAIL"]
                                     except (KeyError, IndexError):
                                         header_template[header]["DATA"].append({"DETAIL": []})
-                                    header_template[header]["DATA"][0]["DETAIL"].append(stock_detail)
-                            if sub_brand["warehouse"] == header[0] and sub_brand["warehouse"] == kino_stock.get("warehouse") \
-                                and sub_brand["branch_code"] != header[1] or sub_brand["entity_code"] != header[2]:
+                                    if stock_detail not in header_template[header]["DATA"][0]["DETAIL"]:
+                                        detail_list = header_template[header]["DATA"][0]["DETAIL"]
+                                        append_or_sum_detail(detail_list, stock_detail)
+                            elif sub_brand["warehouse"] == header[0] and sub_brand["warehouse"] == kino_stock.get("warehouse") \
+                                and (sub_brand["branch_code"] != header[1] or sub_brand["entity_code"] != header[2]):
                                 if kino_stock.get("sub_brand").lower() not in sub_brand["sub_brands"]:
                                     stock_detail = {
                                         "PRDCODE": remove_kn(kino_stock.get("item_code")) if not kino_stock.get("supplier_part_no") else remove_kn(kino_stock.get("supplier_part_no")),
@@ -547,8 +560,10 @@ def create_stock_payload(item_code: str = None, warehouse: str = None,date:str=N
                                         detail = header_template[header]["DATA"][0]["DETAIL"]
                                     except (KeyError, IndexError):
                                         header_template[header]["DATA"].append({"DETAIL": []})
-                                    header_template[header]["DATA"][0]["DETAIL"].append(stock_detail)
-                            if sub_brand["warehouse"] != kino_stock.get("warehouse"):
+                                    if stock_detail not in header_template[header]["DATA"][0]["DETAIL"]:
+                                        detail_list = header_template[header]["DATA"][0]["DETAIL"]
+                                        append_or_sum_detail(detail_list, stock_detail)
+                            elif sub_brand["warehouse"] != kino_stock.get("warehouse"):
                                 stock_detail = {
                                     "PRDCODE": remove_kn(kino_stock.get("item_code")) if not kino_stock.get("supplier_part_no") else remove_kn(kino_stock.get("supplier_part_no")),
                                     "WHLOC1": whloc1,
@@ -560,7 +575,8 @@ def create_stock_payload(item_code: str = None, warehouse: str = None,date:str=N
                                 except (KeyError, IndexError):
                                     header_template[header]["DATA"].append({"DETAIL": []})
                                 if stock_detail not in header_template[header]["DATA"][0]["DETAIL"]:
-                                    header_template[header]["DATA"][0]["DETAIL"].append(stock_detail)
+                                    detail_list = header_template[header]["DATA"][0]["DETAIL"]
+                                    append_or_sum_detail(detail_list, stock_detail)
                     else:
                         stock_detail = {
                             "PRDCODE": remove_kn(kino_stock.get("item_code")) if not kino_stock.get("supplier_part_no") else remove_kn(kino_stock.get("supplier_part_no")),
@@ -572,12 +588,12 @@ def create_stock_payload(item_code: str = None, warehouse: str = None,date:str=N
                             detail = header_template[header]["DATA"][0]["DETAIL"]
                         except (KeyError, IndexError):
                             header_template[header]["DATA"].append({"DETAIL": []})
-                        header_template[header]["DATA"][0]["DETAIL"].append(stock_detail)
+                        if stock_detail not in header_template[header]["DATA"][0]["DETAIL"]:
+                            detail_list = header_template[header]["DATA"][0]["DETAIL"]
+                            append_or_sum_detail(detail_list, stock_detail)
         return header_template
     except Exception:
         raise Exception(traceback.format_exc())
-        
-
 
 def safe_response_json(response):
     try:
@@ -1513,7 +1529,6 @@ if __name__ == '__main__':
     data_dict = {
         "item_code":None,
         "warehouse":None,
-        "date":"2026-04-27"
+        "date":"2026-04-29"
     }
     payload_stock_all_warehouse = post_stock(data=data_dict)
-    # worker_send_invoice(raw_payloads=mock_data_query_invoice(),stock_payload=payload_stock_all_warehouse)
